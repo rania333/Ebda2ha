fedd96d1dd65ac6bb424e395d19d214d7a3c06ac
ashraktmohamed <ashraktmohamed486@gmail.com>
2021-06-22 14:55:59 +0200

    env changes

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -7,6 +7,7 @@ const router = express.Router();
 
 router.get('/',is_Auth, chatControllers.allChats);
 
+
 router.get('/:receiverId',is_Auth, chatControllers.getmessage);
 
 router.post('/:receiverId/message',is_Auth, chatControllers.createMessage);
